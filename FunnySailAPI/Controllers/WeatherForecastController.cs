823e210f400f7ec6cac8693289ba1e200b5f493a
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-22 13:33:07 +0100

    ADD: test

--- FunnySailAPI/Controllers/WeatherForecastController.cs
+++ FunnySailAPI/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace FunnySailAPI.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            string ii = "prueba";
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
