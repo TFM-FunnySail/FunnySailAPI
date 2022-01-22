314748eb1cde59002a04f729a0a3f40e3ed10136
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-22 14:09:52 +0100

    ADD: first migration

---
+++ FunnySailAPI.Infrastructure/CAD/FunnySailDBContext.cs
@@ -0,0 +1,22 @@
+﻿using FunnySailAPI.ApplicationCore.EN.FunnySail;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunnySailAPI.Infrastructure.CAD
+{
+    public class FunnySailDBContext : DbContext
+    {
+        public FunnySailDBContext()
+        {
+
+        }
+        public FunnySailDBContext(DbContextOptions<FunnySailDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BoatType> BoatTypes { get; set; }
+    }
+}
