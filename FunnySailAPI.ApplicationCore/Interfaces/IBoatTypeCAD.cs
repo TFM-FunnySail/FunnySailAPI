dbb8342e8224e5f66780bd2c9ca3fd0f7db7e7c6
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-27 14:30:15 +0100

    ADD: BoatTypeCAD

---
+++ FunnySailAPI.ApplicationCore/Interfaces/IBoatTypeCAD.cs
@@ -0,0 +1,13 @@
+﻿using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnySailAPI.ApplicationCore.Interfaces
+{
+    public interface IBoatTypeCAD : IBaseCAD<BoatTypeEN>
+    {
+        Task<int> AddBoatType(string name, string description);
+    }
+}
---
+++ FunnySailAPI.Infrastructure/CAD/BoatTypeCAD.cs
@@ -0,0 +1,29 @@
+﻿using FunnySailAPI.ApplicationCore.Interfaces;
+using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnySailAPI.Infrastructure.CAD
+{
+    public class BoatTypeCAD : BaseCAD<BoatTypeEN>, IBoatTypeCAD
+    {
+        private ApplicationDbContext _dbContext;
+        public BoatTypeCAD(ApplicationDbContext dbContext) :base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> AddBoatType(string name, string description)
+        {
+            BoatTypeEN boatType = await AddAsync(new BoatTypeEN
+            {
+                Name = name,
+                Description = description
+            });
+
+            return boatType.Id;
+        } 
+    }
+}
