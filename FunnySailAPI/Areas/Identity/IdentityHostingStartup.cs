bd855038b3e7c0ee216d341ae3b4ac4493ff837c
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-29 16:54:34 +0100

    ADD: identity scaffolding

---
+++ FunnySailAPI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FunnySailAPI.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FunnySailAPI.Areas.Identity.IdentityHostingStartup))]
+namespace FunnySailAPI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
