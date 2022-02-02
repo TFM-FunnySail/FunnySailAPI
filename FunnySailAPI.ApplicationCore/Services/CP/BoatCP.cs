1b0c00b41d6affec41205cbba63a405f0a55a2f8
Orlando Marques <realorlanditomadrid@gmail.com>
2022-02-02 11:33:06 +0100

    ADD: BoatInputDTO and BoatOutputDTO

--- FunnySailAPI.ApplicationCore/Services/CP/BoatCP.cs
+++ FunnySailAPI.ApplicationCore/Services/CP/BoatCP.cs
@@ -1,18 +1,51 @@
 ﻿using FunnySailAPI.ApplicationCore.Interfaces.CEN;
 using FunnySailAPI.ApplicationCore.Interfaces.CEN.FunnySail;
+using FunnySailAPI.ApplicationCore.Models.DTO;
+using FunnySailAPI.ApplicationCore.Models.DTO.Input;
+using FunnySailAPI.ApplicationCore.Models.DTO.Output;
+using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FunnySailAPI.ApplicationCore.Services.CP.FunnySail
 {
     public class BoatCP
     {
         private readonly IBoatCEN _boatCEN;
+        private readonly IBoatInfoCEN _boatInfoCEN;
+        private readonly IBoatPricesCEN _boatPricesCEN;
+        private readonly IBoatResourceCEN _boatResourceCEN;
+        private readonly IBoatTypeCEN _boatTypeCEN;
 
-        public BoatCP(IBoatCEN boatCEN)
+        public BoatCP(IBoatCEN boatCEN,
+                      IBoatInfoCEN boatInfoCEN,
+                      IBoatTypeCEN boatTypeCEN,
+                      IBoatResourceCEN boatResourceCEN,
+                      IBoatPricesCEN boatPricesCEN)
         {
             _boatCEN = boatCEN;
+            _boatInfoCEN = boatInfoCEN;
+            _boatPricesCEN = boatPricesCEN;
+            _boatResourceCEN = boatResourceCEN;
+            _boatTypeCEN = boatTypeCEN;
+        }
+
+        public Task<BoatOutputDTO> CreateBoat(AddBoatInputDTO addBoatInput)
+        {
+            //Validar algunos datos
+
+            //Abrir transaccion
+
+            //Crear embarcacion
+
+            //Crear datos de embarcacion
+
+            //Crear precios de embarcacion
+
+            //Crear imagenes de embarcacion
+
         }
     }
 }
