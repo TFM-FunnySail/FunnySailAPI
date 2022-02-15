11ad3d16d3f258fcf5b9344fd598e13cbc5b35a8
Diego Juarez Romero <diegojuarez_13@hotmail.com>
2022-02-15 10:28:15 +0100

    Mod: PortAtributesDB

---
+++ FunnySailAPI.Infrastructure/Migrations/20220214234054_ModPortAtributes.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FunnySailAPI.Infrastructure.Migrations
+{
+    public partial class ModPortAtributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Ports",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Ports",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 1000);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Ports",
+                type: "int",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Location",
+                table: "Ports",
+                type: "int",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+        }
+    }
+}
