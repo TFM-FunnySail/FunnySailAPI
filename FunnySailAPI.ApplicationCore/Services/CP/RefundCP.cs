763951c7943e69bd82b159e3c8c3bb197715a76f
Orlando Marques <realorlanditomadrid@gmail.com>
2022-02-22 13:17:41 +0100

    ADD: CreateRefund method

---
+++ FunnySailAPI.ApplicationCore/Services/CP/RefundCP.cs
@@ -0,0 +1,33 @@
+﻿using FunnySailAPI.ApplicationCore.Interfaces;
+using FunnySailAPI.ApplicationCore.Interfaces.CEN.FunnySail;
+using FunnySailAPI.ApplicationCore.Interfaces.CP.FunnySail;
+using FunnySailAPI.ApplicationCore.Models.DTO.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnySailAPI.ApplicationCore.Services.CP
+{
+    public class RefundCP : IRefundCP
+    {
+        private readonly IRefundCEN _refundCEN;
+        private IDatabaseTransactionFactory _databaseTransactionFactory;
+
+        public RefundCP(IRefundCEN refundCEN,
+                        IDatabaseTransactionFactory databaseTransactionFactory)
+        {
+            _refundCEN = refundCEN;
+            _databaseTransactionFactory = databaseTransactionFactory;
+        }
+
+        public async Task<int> CreateRefund(AddRefundInputDTO addRefundInput)
+        {
+            int newRefund = await _refundCEN.CreateRefund(addRefundInput.BookingId,
+                                                    addRefundInput.description,
+                                                    addRefundInput.amountToReturn);
+
+            return newRefund;
+        }
+    }
+}
