b449885d55034cd66a70010506ecb2fac6b62d26
Orlando Marques <realorlanditomadrid@gmail.com>
2022-02-23 15:53:39 +0100

    ADD: ownerInvoice filters and Get method in BaseCAD

--- FunnySailAPI.ApplicationCore/Interfaces/CAD/FunnySail/IOwnerInvoiceLineCAD.cs
+++ FunnySailAPI.ApplicationCore/Interfaces/CAD/FunnySail/IOwnerInvoiceLineCAD.cs
@@ -1,11 +1,19 @@
-﻿using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+﻿using FunnySailAPI.ApplicationCore.Models.DTO.Filters;
+using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using FunnySailAPI.ApplicationCore.Models.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FunnySailAPI.ApplicationCore.Interfaces.CAD.FunnySail
 {
     public interface IOwnerInvoiceLineCAD: IBaseCAD<OwnerInvoiceLineEN>
     {
+        Task<IList<OwnerInvoiceLineEN>> Get(OwnerInvoiceLineFilters filters,
+                                            Func<IQueryable<OwnerInvoiceLineEN>, IOrderedQueryable<OwnerInvoiceLineEN>> orderBy = null,
+                                            string includeProperties = "",
+                                            Pagination pagination = null);
     }
 }
---
+++ FunnySailAPI.ApplicationCore/Models/DTO/Filters/OwnerInvoiceLineFilters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunnySailAPI.ApplicationCore.Models.DTO.Filters
+{
+    public class OwnerInvoiceLineFilters
+    {
+        public List<int> BookingIds { get; set; }
+    }
+}
