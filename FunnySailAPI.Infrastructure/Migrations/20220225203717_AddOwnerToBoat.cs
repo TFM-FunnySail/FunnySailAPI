9b039b6a7d8369bc3ede73d0a49c4d245e16cc5e
Orlando Marques <realorlanditomadrid@gmail.com>
2022-02-26 20:30:54 +0100

    ADD: OWNER TO boat migration

---
+++ FunnySailAPI.Infrastructure/Migrations/20220225203717_AddOwnerToBoat.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FunnySailAPI.Infrastructure.Migrations
+{
+    public partial class AddOwnerToBoat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Boats",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Boats");
+        }
+    }
+}
