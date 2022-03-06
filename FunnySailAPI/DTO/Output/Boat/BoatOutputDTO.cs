6e7d3386799d34fa2d21c6e3d6f583d8d087cfa3
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-06 11:43:37 +0100

    ADD: MooringOutputDTO and PortOutputDTO

--- FunnySailAPI/DTO/Output/Boat/BoatOutputDTO.cs
+++ FunnySailAPI/DTO/Output/Boat/BoatOutputDTO.cs
@@ -1,4 +1,5 @@
 ﻿using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using FunnySailAPI.DTO.Output.Mooring;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,6 @@ namespace FunnySailAPI.DTO.Output.Boat
         public string Name { get; set; }
         public string Description { get; set; }
         public string Registration { get; set; }
-        public string MooringPoint { get; set; }
         public decimal Length { get; set; }
         public decimal Sleeve { get; set; }
         public int Capacity { get; set; }
@@ -24,6 +24,7 @@ namespace FunnySailAPI.DTO.Output.Boat
         public decimal HourBasePrice { get; set; }
         public float Supplement { get; set; }
         public BoatTypeOutputDTO BoatType { get; set; }
+        public MooringOutputDTO Mooring { get; set; }
         public List<BoatResourcesOutputDTO> BoatResources { get; set; }
         public List<RequiredBoatTitleOutputDTO> RequiredBoatTitles { get; set; }
 
@@ -32,34 +33,5 @@ namespace FunnySailAPI.DTO.Output.Boat
 
         }
 
-        public BoatOutputDTO(BoatEN boatEN)
-        {
-            Id = boatEN.Id;
-            Supplement = boatEN.BoatPrices.Supplement;
-            Active = boatEN.Active;
-            CreatedDate = boatEN.CreatedDate;
-            PendingToReview = boatEN.PendingToReview;
-            BoatType = new BoatTypeOutputDTO
-            {
-                Id = boatEN.BoatType.Id,
-                Name = boatEN.BoatType.Name,
-                Description = boatEN.BoatType.Description,
-            };
-            Capacity = boatEN.BoatInfo.Capacity;
-            Description = boatEN.BoatInfo.Description;
-            Name = boatEN.BoatInfo.Name;
-            MooringPoint = boatEN.BoatInfo.MooringPoint;
-            MotorPower = boatEN.BoatInfo.MotorPower;
-            Length = boatEN.BoatInfo.Length;
-            Sleeve = boatEN.BoatInfo.Sleeve;
-            Registration = boatEN.BoatInfo.Registration;
-            DayBasePrice = boatEN.BoatPrices.DayBasePrice;
-            HourBasePrice = boatEN.BoatPrices.HourBasePrice;
-            RequiredBoatTitles = boatEN.RequiredBoatTitles.Select(x => new RequiredBoatTitleOutputDTO
-            {
-                TitleId = x.TitleId
-            }).ToList();
-            
-        }
     }
 }
---
+++ FunnySailAPI/DTO/Output/Port/PortOutputDTO.cs
@@ -0,0 +1,16 @@
+﻿using FunnySailAPI.DTO.Output.Mooring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunnySailAPI.DTO.Output.Port
+{
+    public class PortOutputDTO
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Location { get; set; }
+        public List<MooringOutputDTO> Moorings { get; set; }
+    }
+}
