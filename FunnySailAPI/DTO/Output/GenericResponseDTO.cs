a9d23188ff2c5e242869ed8072864dfa8a1d5b3f
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-06 12:41:28 +0100

    ADD: GenericResponseDTO.cs

---
+++ FunnySailAPI/DTO/Output/GenericResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunnySailAPI.DTO.Output
+{
+    public class GenericResponseDTO<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Limit { get; set; }
+        public int Offset { get; set; }
+        public int Total { get; set; }
+
+        public GenericResponseDTO(IEnumerable<T> items,int limit, int offset, int total)
+        {
+            Items = items;
+            Limit = limit;
+            Offset = offset;
+            Total = total;
+        }
+    }
+}
