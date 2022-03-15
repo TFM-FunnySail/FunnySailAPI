30236c016aabcee8e7811d62b8cb3e6e93477844
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-15 13:08:44 +0100

    ADD: Bearer token to swagger

--- FunnySailAPI/Startup.cs
+++ FunnySailAPI/Startup.cs
@@ -153,6 +153,16 @@ namespace FunnySailAPI
                         Email = string.Empty
                     },
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
             });
         }
 
@@ -178,8 +188,7 @@ namespace FunnySailAPI
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "FunnySail API V1");
-
-
+                
                 // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
                 c.RoutePrefix = "swagger";
             });
