d814b8c58c31d7a6ae478a723e7c43991ba64e9a
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-22 11:01:03 +0100

    ADD: BoatResourceFilters

---
+++ FunnySailAPI.ApplicationCore/Models/Filters/BoatResourceFilters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunnySailAPI.ApplicationCore.Models.Filters
+{
+    public class BoatResourceFilters
+    {
+        public int BoatId { get; set; }
+        public int ResourceId { get; set; }
+        public List<int> NotResourceId { get; set; }
+        public List<int> NotBoatId { get; set; }
+    }
+}
