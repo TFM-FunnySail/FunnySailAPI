a5a4c44c9dc2fce9187d277eea5e89273752f08a
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-22 19:57:29 +0100

    ADD: porcentOwnerPrice field

---
+++ FunnySailAPI.Infrastructure/Migrations/20220322185657_AddPorcentOwnerInvoice.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FunnySailAPI.Infrastructure.Migrations
+{
+    public partial class AddPorcentOwnerInvoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "PorcentPriceOwner",
+                table: "BoatPrices",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PorcentPriceOwner",
+                table: "BoatPrices");
+        }
+    }
+}
