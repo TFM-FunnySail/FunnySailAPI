d8cc36dbff769e36cb75385c1d950c45e20585ec
Diego Juarez Romero <diegojuarez_13@hotmail.com>
2022-03-23 21:45:02 +0100

    ADD: Service controller

--- FunnySailAPI.ApplicationCore/Interfaces/CEN/FunnySail/IServiceCEN.cs
+++ FunnySailAPI.ApplicationCore/Interfaces/CEN/FunnySail/IServiceCEN.cs
@@ -1,9 +1,14 @@
 ﻿using FunnySailAPI.ApplicationCore.Interfaces.CAD.FunnySail;
 using FunnySailAPI.ApplicationCore.Models.DTO.Input;
+using FunnySailAPI.ApplicationCore.Models.DTO.Input.Sercices;
 using FunnySailAPI.ApplicationCore.Models.DTO.Input.Services;
+using FunnySailAPI.ApplicationCore.Models.Filters;
 using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using FunnySailAPI.ApplicationCore.Models.Utils;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +17,15 @@ namespace FunnySailAPI.ApplicationCore.Interfaces.CEN.FunnySail
     public interface IServiceCEN
     {
         Task<int> CreateService(string name, decimal price, string description);
+        Task<int> CreateService(AddServiceDTO serviceInput);
         Task<ServiceEN> UpdateService(UpdateServiceDTO updateServiceInput);
         Task DeleteService(int id);
         IServiceCAD GetServiceCAD();
+        Task<IList<ServiceEN>> GetAll(ServiceFilters filters = null,
+        Pagination pagination = null,
+        Func<IQueryable<ServiceEN>, IOrderedQueryable<ServiceEN>> orderBy = null,
+        Func<IQueryable<ServiceEN>, IIncludableQueryable<ServiceEN, object>> includeProperties = null);
+        Task<int> GetTotal(ServiceFilters filters = null);
+
     }
 }
---
+++ FunnySailAPI/DTO/Output/Service/ServiceOutputDTO.cs
@@ -0,0 +1,25 @@
+﻿using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using FunnySailAPI.DTO.Output.Mooring;
+using FunnySailAPI.DTO.Output.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunnySailAPI.DTO.Output.Service
+{
+    public class ServiceOutputDTO
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public bool Active { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public ServiceOutputDTO()
+        {
+
+        }
+
+    }
+}
