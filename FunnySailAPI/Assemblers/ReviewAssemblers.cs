d7af5b04811ccdb18944bd4e2ea244882e3a8808
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-23 17:45:59 +0100

    ADD: ReviewAssemblers

---
+++ FunnySailAPI/Assemblers/ReviewAssemblers.cs
@@ -0,0 +1,24 @@
+﻿using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using FunnySailAPI.DTO.Output.Review;
+
+namespace FunnySailAPI.Assemblers
+{
+    public static class ReviewAssemblers
+    {
+        public static ReviewOutputDTO Convert(ReviewEN review)
+        {
+            ReviewOutputDTO reviewOutputDTO = new ReviewOutputDTO
+            {
+                Id = review.Id,
+            };
+
+            if(review.Admin != null)
+                reviewOutputDTO.Admin = UserAssemblers.Convert(review.Admin);
+
+            if (review.Boat != null)
+                reviewOutputDTO.Boat = BoatAssemblers.Convert(review.Boat);
+
+            return reviewOutputDTO;
+        }
+    }
+}
