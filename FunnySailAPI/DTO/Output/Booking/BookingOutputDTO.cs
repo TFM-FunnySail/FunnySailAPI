c1383323dc2b3fe570b1b6e83942a63d468b34b5
andresll706 <andresll706@gmail.com>
2022-03-23 19:38:24 +0100

    ADD: BookingOutputDTO

--- FunnySailAPI/DTO/Output/Booking/BookingOutputDTO.cs
+++ FunnySailAPI/DTO/Output/Booking/BookingOutputDTO.cs
@@ -7,7 +7,14 @@ namespace FunnySailAPI.DTO.Output.Booking
 {
     public class BookingOutputDTO
     {
-
+        public int Id { get; set; }
+        public string ClientId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public int TotalPeople { get; set; }
+        public bool Paid { get; set; }
+        public bool RequestCaptain { get; set; }
         public BookingOutputDTO() { }
     }
 }
