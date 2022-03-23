5ff7def557085a1ce2f1e5f17920de3cc18ab869
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-23 17:25:24 +0100

    ADD: ReviewOutputDTO

---
+++ FunnySailAPI/DTO/Output/Review/ReviewOutputDTO.cs
@@ -0,0 +1,14 @@
+﻿using FunnySailAPI.DTO.Output.Boat;
+using FunnySailAPI.DTO.Output.User;
+
+namespace FunnySailAPI.DTO.Output.Review
+{
+    public class ReviewOutputDTO
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public BoatOutputDTO Boat { get; set; }
+        public UserOutputDTO Admin { get; set; }
+    }
+}
