fe04ee9ea0e0da140a7026cec8fa13986037cb14
andresll706 <andresll706@gmail.com>
2022-03-24 18:56:38 +0100

    ADD: PortAssemblers

---
+++ FunnySailAPI/Assemblers/PortAssemblers.cs
@@ -0,0 +1,30 @@
+﻿using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using FunnySailAPI.DTO.Output.Port;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunnySailAPI.Assemblers
+{
+    public class PortAssemblers
+    {
+        public static PortOutputDTO Convert(PortEN portEN)
+        {
+            PortOutputDTO portOutputDTO = new PortOutputDTO
+            {
+                Id = portEN.Id,
+                Location = portEN.Location,
+                Name = portEN.Name,
+            };
+
+            if (portEN.Moorings != null) 
+            {
+                foreach (var mooring in portEN.Moorings)
+                {
+                    //portOutputDTO.Moorings.Add(MooringAssembler.Convert(mooring));
+                }
+            }
+            return portOutputDTO;
+        }
+}
