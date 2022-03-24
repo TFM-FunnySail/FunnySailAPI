8bccb6f47a5e2a9e2861d8a2206ce26a0788f1a3
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-24 18:35:30 +0100

    ADD: SignInManagerMock

---
+++ UnitTest/FakeFactories/SignInManagerMock.cs
@@ -0,0 +1,43 @@
+﻿using FunnySailAPI.ApplicationCore.Models.FunnySailEN;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.FakeFactories
+{
+    public class SignInManagerMock
+    {
+        public Mock<SignInManager<ApplicationUser>> singInManager;
+        public SignInManagerMock()
+        {
+            var userManager = new UserManagerMock();
+
+            singInManager = new Mock<SignInManager<ApplicationUser>>(userManager.userManager,
+                 new Mock<IHttpContextAccessor>().Object,
+                 new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>().Object,
+                 new Mock<IOptions<IdentityOptions>>().Object,
+                 new Mock<ILogger<SignInManager<ApplicationUser>>>().Object,
+                 new Mock<IAuthenticationSchemeProvider>().Object);
+        }
+
+        public void SetupForLoginPassSuccess()
+        {
+            singInManager.Setup(x => x.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Success);
+        }
+
+        public void SetupForLoginPassFailed()
+        {
+            singInManager.Setup(x => x.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Failed);
+        }
+    }
+}
