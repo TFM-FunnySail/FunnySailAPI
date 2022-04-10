bb880a8df46040b035d6d1b0e555a5e023e6f1c4
Orlando Marques <realorlanditomadrid@gmail.com>
2022-04-10 11:39:26 +0200

    ADD: activities to database

--- FunnySailAPI.Infrastructure/Initialize/InitializeDB.cs
+++ FunnySailAPI.Infrastructure/Initialize/InitializeDB.cs
@@ -40,32 +40,65 @@ namespace FunnySailAPI.Infrastructure.Initialize
                     _dbContext.Database.Migrate();
                 }
 
-                if (_dbContext.Roles.Any(ro => ro.Name == UserRoleEnum.Admin.ToString())) return;
-
-                await _roleManager.CreateAsync(new IdentityRole(UserRolesConstant.CLIENT));
-                await _roleManager.CreateAsync(new IdentityRole(UserRolesConstant.ADMIN));
-                await _roleManager.CreateAsync(new IdentityRole(UserRolesConstant.BOAT_OWNER));
+                if (!_dbContext.Roles.Any(ro => ro.Name == UserRoleEnum.Admin.ToString()))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(UserRolesConstant.CLIENT));
+                    await _roleManager.CreateAsync(new IdentityRole(UserRolesConstant.ADMIN));
+                    await _roleManager.CreateAsync(new IdentityRole(UserRolesConstant.BOAT_OWNER));
+                }
 
                 //Crear usuarios
-                (IdentityResult result, ApplicationUser user) = await _unitOfWork.UserCP.CreateUser(new AddUserInputDTO
+                string email = "admin1@funnysail.com";
+                var user1 = await _userManager.FindByEmailAsync(email);
+                if (user1 == null)
                 {
-                    Email = "admin1@funnysail.com",
-                    ConfirmPassword = "Admin1**23*er",
-                    FirstName = "Admin",
-                    LastName = "Admin Last Name",
-                    Password = "Admin1**23*er",
-                    PhoneNumber = "5555555"
-                });
-                user.EmailConfirmed = true;
-                await _dbContext.SaveChangesAsync();
-                await _userManager.AddToRoleAsync(user, UserRolesConstant.ADMIN);
+                    (IdentityResult result, ApplicationUser user) = await _unitOfWork.UserCP.CreateUser(new AddUserInputDTO
+                    {
+                        Email = "admin1@funnysail.com",
+                        ConfirmPassword = "Admin1**23*er",
+                        FirstName = "Admin",
+                        LastName = "Admin Last Name",
+                        Password = "Admin1**23*er",
+                        PhoneNumber = "5555555"
+                    });
+                    user.EmailConfirmed = true;
+                    await _dbContext.SaveChangesAsync();
+                    await _userManager.AddToRoleAsync(user, UserRolesConstant.ADMIN);
+                }
+                
+                if(!await _dbContext.Activity.AnyAsync())
+                {
+                    int index = 1;
+                    List<ActivityEN> activities = new List<ActivityEN>();
+                    for (; index <= 50; index++)
+                    {
+                        bool active = NextFloat(0, 1) > (decimal)0.5;
+                        activities.Add(new ActivityEN
+                        {
+                            Active = active,
+                            Description = $"Actividad de prueba {index}",
+                            ActivityDate = DateTime.UtcNow,
+                            Name = $"Actividad {index}",
+                            Price = NextFloat(1, 100)
+                        });
+                    }
+                    await _dbContext.Activity.AddRangeAsync(activities);
+                    await _dbContext.SaveChangesAsync();
+                }
             }
             catch (Exception)
             {
 
             }
         }
+        private decimal NextFloat(float min, float max)
+        {
+            System.Random random = new System.Random();
+            double val = (random.NextDouble() * (max - min) + min);
+            return (decimal)val;
+        }
     }
+
 
     public interface IInitializeDB
     {
