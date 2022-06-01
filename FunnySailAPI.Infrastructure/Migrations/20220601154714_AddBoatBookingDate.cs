0a7f6ce83d7091c11fc12b7a75111805b875279c
Orlando Marques <realorlanditomadrid@gmail.com>
2022-06-01 17:47:48 +0200

    ADD: date boat booking migration

---
+++ FunnySailAPI.Infrastructure/Migrations/20220601154714_AddBoatBookingDate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FunnySailAPI.Infrastructure.Migrations
+{
+    public partial class AddBoatBookingDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EntryDate",
+                table: "Bookings",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DepartureDate",
+                table: "Bookings",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DepartureDate",
+                table: "BoatBooking",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EntryDate",
+                table: "BoatBooking",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartureDate",
+                table: "BoatBooking");
+
+            migrationBuilder.DropColumn(
+                name: "EntryDate",
+                table: "BoatBooking");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EntryDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DepartureDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
