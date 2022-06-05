761f269bfa751dbe63e73f899cb2fc3c89231210
Orlando Marques <realorlanditomadrid@gmail.com>
2022-06-05 14:38:05 +0200

    ADD: remove activityDate field in activity output

--- FunnySailAPI/DTO/Output/Activity/ActivityOutputDTO.cs
+++ FunnySailAPI/DTO/Output/Activity/ActivityOutputDTO.cs
@@ -11,7 +11,6 @@ namespace FunnySailAPI.DTO.Output.Activity
     public class ActivityOutputDTO
     {
         public int Id { get; set; }
-        public DateTime ActivityDate { get; set; }
         public String Name { get; set; }
         public bool Active { get; set; }
         public decimal Price { get; set; }
